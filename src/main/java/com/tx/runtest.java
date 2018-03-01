0a2346faef85c31f8327c9a49625014f465aff54
sakthi <shaktiganesh@gmail.com>
2018-03-01 17:39:15 +0530

    testng

--- src/main/java/com/tx/runtest.java
+++ src/main/java/com/tx/runtest.java
@@ -8,13 +8,32 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
 
 public class runtest {
 
 
 	private static RemoteWebDriver driver;
 
-	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+//	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+//		 DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+//	        desiredCapabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+//			desiredCapabilities.setCapability("deviceName",	 "Nexus");
+//			desiredCapabilities.setCapability("udid", "00a000c85c98c2b1");
+//			desiredCapabilities.setCapability("platformVersison", "5.1");
+//			desiredCapabilities.setCapability("platformName", "Android");
+//			desiredCapabilities.setCapability("appPackage", "de.taxfix");
+//			desiredCapabilities.setCapability("appActivity", "de.taxfix.SplashActivity");
+//			driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"),desiredCapabilities);
+//			driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+//			Thread.sleep(10000);
+//			driver.findElement(By.xpath("//android.widget.TextView[@text='Start your tax return']")).click();
+//			//driver.quit();
+//
+//	}
+//	
+	@Test
+	public void appTest(){
 		 DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 	        desiredCapabilities.setCapability(CapabilityType.BROWSER_NAME, "");
 			desiredCapabilities.setCapability("deviceName",	 "Nexus");
@@ -23,12 +42,23 @@ public class runtest {
 			desiredCapabilities.setCapability("platformName", "Android");
 			desiredCapabilities.setCapability("appPackage", "de.taxfix");
 			desiredCapabilities.setCapability("appActivity", "de.taxfix.SplashActivity");
-			driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"),desiredCapabilities);
+			try {
+				driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"),desiredCapabilities);
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
-			Thread.sleep(10000);
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			driver.findElement(By.xpath("//android.widget.TextView[@text='Start your tax return']")).click();
 			//driver.quit();
 
+		
 	}
 
 }
